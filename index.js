86dafe766b4227705d5ce34d3107e55843b3e5f2
RamliWane <ramlisilawane865@gmail.com>
2025-09-24 21:43:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+// app.use("/", (req, res, next) => {
+//     res.send("Hello World");
+// });
+
+app.get("/", (req, res) => {
+    res.send("Hello GET");
+});
+
+app.post("/", (req, res) => {
+    res.send("Hello POST");
+});
+
+app.listen(4000, () => {
+    console.log('Server berjalan di port 4000');
+});
