46ac6a5a5049fadb6a5fc64bf521120850d18b58
RamliWane <ramlisilawane865@gmail.com>
2025-09-26 22:17:02 +0700

    connect to mysql

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-const express = require('express');
+    const express = require('express');
+    
+    const mysql = require('mysql2');
 
-const UserRoutes = require('./routes/users');
+    const UserRoutes = require('./routes/users');
 
-const LogsRequestMiddleware = require('./middleware/logs');
+    const LogsRequestMiddleware = require('./middleware/logs');
 
-const app = express();
+    const dbPool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'express_mysql',
+    });
+
+    const app = express(); 
+
+    app.use(LogsRequestMiddleware.LogsRequest);
+    app.use(express.json());
+
+    app.use("/users", UserRoutes);
+
+    app.use('/', (req, res) => {
+        dbPool.execute('SELECT * FROM users', (err, rows) => {
+            if(err) {
+                res.json({
+                    message : "terjadi kesalahan",
+                })
+            }
+            res.json({
+                message: "berhasil koneksi ke database",
+                data: rows,
+            })
+
+        })
+    });
 
 
-app.use(LogsRequestMiddleware.LogsRequest);
-
-app.use(express.json());
-
-app.use("/users", UserRoutes);
-
-
-app.listen(4000, () => {
-    console.log('Server berjalan di port 4000');
-});
+    app.listen(4000, () => {
+        console.log('Server berjalan di port 4000');
+    });
